20ec9458d5a138e7d3eca51db865e82bf6a26b6d
Maanit <maanitarora1234@gmail.com>
2025-04-07 23:12:22 +0530

    initial commit

---
+++ app/nlp.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.indices.postprocessor import SimilarityPostprocessor
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+load_dotenv(override=True)
+print("API Key:", os.getenv("OPENAI_API_KEY"))
+
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not found in environment variables or .env file.")
+os.environ["OPENAI_API_KEY"] = api_key
+
+
+def initialize_query_engine():
+    folder = 'uploaded_files'
+    
+    if not os.listdir(folder):
+        print("No files found in the uploaded_files directory for NLP model initialization")
+        return None
+
+    try:
+        documents = SimpleDirectoryReader(folder).load_data()
+        index = VectorStoreIndex.from_documents(documents, show_progress=True)
+        
+        retriever = VectorIndexRetriever(index=index, similarity_top_k=4)
+        postprocessor = SimilarityPostprocessor(similarity_cutoff=0.70)
+        
+        query_engine = RetrieverQueryEngine(
+            retriever=retriever,
+            node_postprocessors=[postprocessor]
+        )
+        print("NLP model initialized successfully")
+        return query_engine
+    except Exception as e:
+        print(f"Error initializing NLP model: {e}")
+        return None
+
+
+# print(response)
+# print(type  (response))
+# a=str(response)
+# print(a)
+# print(type(a))
